cb9054580e618fb29228783fa9cd728dfc768910
Ajay Kumar <102378038+ajaykumar2pp@users.noreply.github.com>
2023-06-16 18:06:22 +0530

    add section

--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import InputControl from '../InputControl/InputControl'
+import styles from "./Editor.module.css"
 
 const Editor = (props) => {
+  const sections = props.sections
   return (
-    <div>
-        <InputControl />
+    <div className="container d-flex flex-column mb-3 mt-4 ">
+      <div className="row shadow p-3">
+        <div className="col-md-12 d-flex justify-content-between ">
+        {Object.keys(sections)?.map((key) => (
+          <div className="d-flex  mb-3" key={key}>
+            {sections[key]}
+            
+          </div>
+        )) }
+        
+        </div>
+        <hr class="border border-danger border-2 opacity-50"></hr>
+      </div>
+   
     </div>
+
+
   )
 }
 
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,41 +1,58 @@
 import React from 'react'
 import Editor from '../../Editor/Editor'
-import Resume from '../../Resume/Resume'
+// import Resume from '../../Resume/Resume'
 import styles from "./Body.module.css";
-import {BsArrowDown} from "react-icons/bs";
+import { BsArrowDown } from "react-icons/bs";
 
 const Body = () => {
 
   const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
 
+  const sections = {
+    basicInfo: "Basic Info",
+    workExp: "Working Experience",
+    project: "Project",
+    education: "Education",
+    achievement: "Achievements",
+    summary: "Summary",
+    other: "Other",
+  }
+
   return (
-    <div class="container my-4">
+    <div className="container my-4">
       <div className="row">
         <div className="col-md-12">
-          <h3 class="text-center">Resume Builder</h3>
+          <h3 className="text-center">Resume Builder</h3>
         </div>
       </div>
       <div className="row">
         <div className="col-md-12">
-          <div className="colors">
+          <div className="colors ">
             <div className={styles.colors}>
               {colors.map((item) => (
-                <span 
-                key={item}
-                style={{backgroundColor:item}}
-                className={styles.color}
-                
+                <span
+                  key={item}
+                  style={{ backgroundColor: item }}
+                  className={styles.color}
+
                 />
               ))}
             </div>
-             <div className='align-center'>
-             <button className="btn btn-primary float-end mt-3 py-2 px-4">Download <BsArrowDown className='h5 ' /></button>
-             </div>
+            <div className=''>
+              <button className="btn btn-primary float-end mt-3 py-2 px-4 align-items-center">Download <BsArrowDown className='h5 ' /></button>
+            </div>
           </div>
         </div>
       </div>
+      
+      <div className="">
+        <Editor sections={sections} />
+      </div>
 
-      <Resume />
+      {/* <div className='d-flex flex-column mb-3'>
+     <Editor sections={sections} />
+     </div> */}
+
     </div>
   )
 }
